81e49b9f823bec3e94333a6752c1b3d4febba997
Won Gu Kang <kangwo1@gmail.com>
2014-02-08 15:15:31 -0500

    modified Stats Monitor file to follow some style guides for scala

---
+++ servers/admin/app/models/StatsMonitor.scala
@@ -0,0 +1,45 @@
+package models
+
+trait StatsMonitor {
+
+  /**
+   * Get RAM usage in percent
+   */
+  def getRamUsage(): Double
+
+  /**
+   * Get CPU usage in percent
+   */
+  def getCpuUsage(): Double
+
+  /**
+   * Get RAM usage in percent for particular process
+   *
+   * @param pid
+   * 			process id of given process
+   */
+  def getProcessRamUsage(pid: Int): Double
+
+  /**
+   * Get CPU usage in percent for particular process
+   *
+   * @param pid
+   * 			process id of given process
+   */
+  def getProcessCpuUsage(pid: Int): Double
+
+  /**
+   * Get total Disk Space
+   */
+  def getTotalDiskSpace(): Long
+
+  /**
+   * Get total Disk Space used
+   */
+  def getUsedDiskSpace(): Long
+
+  /**
+   * Get total free Disk Space
+   */
+  def getFreeDiskSpace(): Long
+}
