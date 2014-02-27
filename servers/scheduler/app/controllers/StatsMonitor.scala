5d8e75e1910539b6ef56f55bbb4dd16c14590e72
Won Gu Kang <kangwo1@gmail.com>
2014-02-27 01:10:55 -0500

    [PDIOMON-5] Added some comments about basic implementation on scheduler server. Also testing something with github....

--- servers/scheduler/app/controllers/StatsMonitor.scala
+++ servers/scheduler/app/controllers/StatsMonitor.scala
@@ -2,6 +2,9 @@ package io.prediction.scheduler
 
 import play.api.mvc._
 
+// Hadoop -> Use metrics2 api
+// Mongodb -> Use command line + javascript script
+
 object StatsMonitor extends Controller {
 
   /**
@@ -13,11 +16,14 @@ object StatsMonitor extends Controller {
    * Get CPU usage in percent
    */
   def getCpuUsage = TODO
+  //NOTE: Currently only monitors the java processes in the scheduler server
+  
 
   /**
    * Get total Disk Space
    */
   def getTotalDiskSpace() = TODO
+  // Needed?
 
   /**
    * Get total Disk Space used
@@ -28,4 +34,6 @@ object StatsMonitor extends Controller {
    * Get total free Disk Space
    */
   def getFreeDiskSpace() = TODO
+  // Needed?
+  
 }
