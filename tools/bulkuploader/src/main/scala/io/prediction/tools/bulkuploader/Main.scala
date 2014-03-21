84d51bb881ea643a98dc2348dc22b62876e02e44
Ronen Azachi <roninio@users.noreply.github.com>
2014-03-21 11:29:46 +0200

    added creation of u2i

--- tools/bulkuploader/src/main/scala/io/prediction/tools/bulkuploader/Main.scala
+++ tools/bulkuploader/src/main/scala/io/prediction/tools/bulkuploader/Main.scala
@@ -2,25 +2,10 @@ package io.prediction.tools.bulkuploader
 
 /**
  * Run from root directory.
- *   % sbt "project toolsBulkUploader" run 
- *
- */
-
-
-import io.prediction.commons.Config
-import io.prediction.commons.appdata.Item
-import com.github.nscala_time.time.Imports.DateTime
-
-object BulkUploader {
-  def main(args: Array[String]) {
-    println("Bulkuploader!")
-    val appid = 2package io.prediction.tools.bulkuploader
-
-/**
- * Run from root directory.
  *   % sbt "project toolsBulkUploader" run
  *
- *   Create number of items, number of users, u2i action automatically. 
+ *   Automatic creation:
+ *   Create number of items, number of users, u2i action automatically.
  *   To disable automatic creation set numUsers, numItems to 0
  */
 
@@ -28,6 +13,8 @@ import io.prediction.commons.Config
 import io.prediction.commons.appdata.Item
 import io.prediction.commons.appdata.User
 import io.prediction.commons.appdata.U2IAction
+import scala.util.Random
+import scala.language.postfixOps
 
 import com.github.nscala_time.time.Imports.DateTime
 
@@ -36,9 +23,10 @@ object BulkUploader {
     println("Bulkuploader!")
     val appid = 1
     val numUsers = 20
-    val numItems = 4
+    val numItems = 90
     makeItem(appid, numItems)
     makeUser(appid, numUsers)
+    makeU2Iaction(appid, numUsers, numItems)
   }
 
   def makeItem(appid: Int, numItems: Int = 0): Any = {
@@ -71,7 +59,7 @@ object BulkUploader {
 
   }
 
-  def makeUser(appid: Int, numUsers: Int = 10) {
+  def makeUser(appid: Int, numUsers: Int = 10): Any = {
     val config = new Config()
     val users = config.getAppdataUsers()
 
@@ -82,22 +70,25 @@ object BulkUploader {
       latlng = None,
       inactive = None,
       attributes = None);
-    if (numItems > 0) {
+    if (numUsers > 0) {
       for (a <- 1 to numUsers) {
         //println( "Value of a: " + a );
+
+        //val r = range(rnd.nextInt(range length))
+        //println("Value of a: " + randomInt);
         users.insert(makeUser("u" + a))
       }
-      reutrn Some(0)
+      return Some(0)
     }
     users.insert(makeUser("name1"))
     users.insert(makeUser("name2"))
   }
 
-  def makeU2Iaction(appid: Int, numUsers: Int = 10) {
+  def makeU2Iaction(appid: Int, numUsers: Int = 0, numItems: Int = 0) {
     val config = new Config()
-    val users = config.getAppdataU2IActions()
+    val u2i = config.getAppdataU2IActions()
 
-    def makeU2IAction(iid: String, uid: String, action: String): U2IAction = U2IAction(
+    def makeU2IAction(uid: String, iid: String, action: String): U2IAction = U2IAction(
       appid = appid,
       action = action,
       uid = uid,
@@ -108,33 +99,16 @@ object BulkUploader {
       price = None);
 
     for (a <- 1 to numUsers) {
+      val rnd = new scala.util.Random
+      val range = 1 to numItems
+      var i = range(rnd.nextInt(range length))
       println("Value of a: " + a);
-      //users.insert(makeUser("u" + a))
+      u2i.insert(makeU2IAction("u" + a, "i" + i, "view"))
+      i = range(rnd.nextInt(range length))
+      u2i.insert(makeU2IAction("u" + a, "i" + i, "view"))
+      i = range(rnd.nextInt(range length))
+      u2i.insert(makeU2IAction("u" + a, "i" + i, "conversion"))
     }
 
-  }
-}
-
-
-    val config = new Config()
-    val items = config.getAppdataItems()
-
-    def makeItem(iid: String, itypes: Seq[String]): Item =
-      Item(
-        id = iid,
-        appid = appid,
-        ct = DateTime.now,
-        itypes = itypes,
-        starttime = None,
-        endtime = None,
-        price = None,
-        profit = None,
-        latlng = None,
-        inactive = None,
-        attributes = None
-      );
-
-    items.insert(makeItem("item1", List("organic", "beef")))
-    items.insert(makeItem("item2", List("beef")))
   }
 }
