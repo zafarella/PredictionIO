8e4da1f88162fa38af91470a0d1665de1e700ad9
Donald Szeto <donald@prediction.io>
2015-02-02 17:02:55 -0800

    Reverse priority of environmental and command line arguments

--- tools/src/main/scala/Agent.scala
+++ tools/src/main/scala/Agent.scala
@@ -79,24 +79,24 @@ object Agent extends Logging {
   }
 
   def start(ca: ConsoleArgs): Int = {
-    val url = sys.env.get("PIO_AGENT_URL").getOrElse {
-      ca.common.agentUrl.getOrElse {
+    val url = ca.common.agentUrl.getOrElse {
+      sys.env.get("PIO_AGENT_URL").getOrElse {
         error("URL not found from configuration file nor command line. " +
           "Aborting.")
         return 1
       }
     }
 
-    val hostname = sys.env.get("PIO_AGENT_HOSTNAME").getOrElse {
-      ca.common.agentHostname.getOrElse {
+    val hostname = ca.common.agentHostname.getOrElse {
+      sys.env.get("PIO_AGENT_HOSTNAME").getOrElse {
         error("Hostname not found from configuration file nor command line. " +
           "Aborting.")
         return 1
       }
     }
 
-    val secret = sys.env.get("PIO_AGENT_SECRET_KEY").getOrElse {
-      ca.common.agentSecret.getOrElse {
+    val secret = ca.common.agentSecret.getOrElse {
+      sys.env.get("PIO_AGENT_SECRET_KEY").getOrElse {
         error("Secret key not found from configuration file nor command line." +
           " Aborting.")
         return 1
@@ -108,6 +108,7 @@ object Agent extends Logging {
       ca.common.pioHome.get)
     val agent = new AgentWebSocketClient(uri, ca.common.pioHome.get)
     if (uri.getScheme == "wss") {
+      info("Enabling SSL")
       val sslContext = SSLContext.getInstance("TLS")
       sslContext.init(null, null, null)
       val factory = sslContext.getSocketFactory
